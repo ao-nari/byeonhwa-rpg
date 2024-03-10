9b0c25f6d8ee797a0deaed7da3bce82fadcb8646
aonari <aonari.contact@gmail.com>
2024-03-10 22:56:40 +0100

    ajout des dossiers et fichiers de base & fonction de tri du bottin fonctionnelle

---
+++ bottin/bottin.js
@@ -0,0 +1,32 @@
+// fonction de tri du bottin
+
+// chargement du DOM 
+document.addEventListener('DOMContentLoaded', function() {
+  const sortingRow = document.querySelector('.sorting-row'); // container ligne de tri 
+
+  sortingRow.addEventListener('click', function(event) {
+    if (event.target.classList.contains('sort-by')) {
+      const columnIndex = event.target.getAttribute('data-column-index'); 
+      sortItems(columnIndex); 
+    }
+  });
+
+  function sortItems(columnIndex) {
+    const itemsCont = document.querySelector('.items-row-container'); // container de toutes les lignes de contenu
+    const itemRows = document.querySelectorAll('.item-row'); // TOUTES les lignes
+    const itemRowsArray = Array.from(itemRows); // converti en array
+
+    // tri selon le texte
+    itemRowsArray.sort((a, b) => {
+      const aValue = a.querySelector(`span.${columnIndex}`).textContent.toLowerCase();
+      const bValue = b.querySelector(`span.${columnIndex}`).textContent.toLowerCase();
+      return aValue.localeCompare(bValue);
+    });
+ 
+    itemsCont.innerHTML = ''; 
+
+    itemRowsArray.forEach(itemRow => {
+      itemsCont.appendChild(itemRow); 
+    });
+  } 
+}); 
